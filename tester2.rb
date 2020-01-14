881df8f041e0dd1fef0d801e8e04d3985bce5708
raquibbi <raquibbi@gmail.com>
2020-01-14 04:16:25 +0000

    finished luhn

---
+++ tester2.rb
@@ -0,0 +1,18 @@
+
+digits = number.to_s.split('').map { |str| str.to_i}
+digits = digits.reverse.map.with_index { |digit, index| index % 2 == 1? digit * 2 : digit }
+
+digits.each_with_index do |digit, index|
+  if digit >= 10
+    digits[index] = digit - 9
+  end
+end
+
+sum = digits.reduce {|digit1, digit2| digit1 + digit2}
+
+if sum % 10 == 0 
+  return true
+else
+  return false
+end
+
